cfcb3c6cdd7edc2d73250996daa40c869e3af410
thenoblepickle <aubrunnerkorbinian3@gmail.com>
2026-02-06 19:11:04 +0100

    Add simple timeline

--- docs/js/index.js
+++ docs/js/index.js
@@ -1,1 +1,5 @@
-gsap.to(".circle", {rotation: 360, x: 100, duration: 1});
+let tl = gsap.timeline({delay:1, repeat:-1, yoyo:true});
+tl.to('.c1', {duration:0.5, x:945, ease:'expo.out'}, .3)
+    .add("squareSpin", "-=0.5")
+    .to('.s1', {duration:2, rotate:360, x:945, ease:'expo.out'}, "squareSpin")
+    .to('.c3', {duration:2.5, x:945, ease:'expo.out'}, "+=0.5");
