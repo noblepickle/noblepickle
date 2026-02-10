4fd63ce4cf935afb5eb03e9b958b05b6d104b237
thenoblepickle <aubrunnerkorbinian3@gmail.com>
2026-02-10 01:22:15 +0100

    scroll trigger

--- docs/js/index.js
+++ docs/js/index.js
@@ -1,5 +1,16 @@
-let tl = gsap.timeline({delay:1, repeat:-1, yoyo:true});
-tl.to('.c1', {duration:0.5, x:945, ease:'expo.out'}, .3)
-    .add("squareSpin", "-=0.5")
-    .to('.s1', {duration:2, rotate:360, x:945, ease:'expo.out'}, "squareSpin")
-    .to('.c3', {duration:2.5, x:945, ease:'expo.out'}, "+=0.5");
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.to(".c", {
+    scrollTrigger: {
+        trigger: ".c",
+        start: "top center",
+        end: "top 100px",
+        scrub: true,
+        pin: ".b",
+        markers: true
+    },
+    x: 400,
+    rotation: 360,
+    ease: "none",
+    duration: 3
+});
